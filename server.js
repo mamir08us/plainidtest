013c9147eb552bb3eeb429e3a2e75f61573e25cf
mamir08us <72876456+mamir08us@users.noreply.github.com>
2025-08-27 19:01:56 -0400

    disable tls

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// ❗ DEV ONLY: disable TLS cert validation (self-signed cert bypass)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const express = require('express');
 const axios = require('axios');
 const session = require('express-session');
