84381a1e9e6667100e2ff9a80227a8ece205935d
mamir08us <72876456+mamir08us@users.noreply.github.com>
2025-08-27 14:24:29 -0400

    Update IP

---
+++ public/plainid notes.rb
@@ -0,0 +1,29 @@
+create view users as
+select
+    id as entity_id,
+    username as user_name,
+    email as user_email,
+    role as user_role,
+    password
+from CRUD.users;
+
+
+
+
+SERVER_SSL_ENABLED:true # enable/disable SSL.
+
+SERVER_SSL_KEY_STORE_PATH # the path to the key store that contains the SSL certificate.
+
+SERVER_SSL_KEY_STORE_PASSWORD # the password used to access the key store.
+
+SERVER_SSL_KEY_STORE_TYPE # the type of the key store (JKS or PKCS12).
+
+SERVER_SSL_KEY_ALIAS # the alias that identifies the key in the key store.
+
+SERVER_SSL_KEY_PASSWORD # the password used to access the key in the key store.
+ 
+
+TNT4J9WTML7W5Q8
+ 
+FvtQnofHBObxwhDuFWECytnudUG5ZYg3XHmZEod6
+ 
